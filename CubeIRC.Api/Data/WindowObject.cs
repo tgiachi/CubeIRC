822ee2e20c99f8f3d12994415a197e3ae7dd9200
tommaso giachi <squid@stormwind.it>
2019-11-16 14:01:23 +0100

    First commit

---
+++ CubeIRC.Api/Data/WindowObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CubeIRC.Api.Data
+{
+    public class WindowObject
+    {
+        public string WindowTitle { get;set; }
+
+        public object Content { get; set; }
+    }
+}
---
+++ CubeIRC.Client/Bootstrap/CubeIrcBootstrapper.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Autofac;
+using Caliburn.Micro;
+using CubeIRC.Api.Utils;
+using CubeIRC.Client.ViewModels;
+using IContainer = Autofac.IContainer;
+
+namespace CubeIRC.Client.Bootstrap
+{
+    public class CubeIrcBootstrapper : BootstrapperBase
+    {
+        private static IContainer _container;
+
+        public CubeIrcBootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void Configure()
+        {
+
+            AssemblySource.Instance.Clear();
+            AssemblySource.Instance.AddRange(AssemblyUtils.GetAppAssemblies());
+
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<WindowManager>()
+                .AsImplementedInterfaces()
+                .SingleInstance();
+
+            builder.RegisterType<EventAggregator>()
+                .AsImplementedInterfaces()
+                .SingleInstance();
+
+            builder.RegisterAssemblyTypes(AssemblyUtils.GetAppAssembliesArray())
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .Where(type => !(string.IsNullOrWhiteSpace(type.Namespace)) && type.Namespace.EndsWith("ViewModels"))
+                .Where(type => type.GetInterface(typeof(INotifyPropertyChanged).Name) != null)
+                .AsSelf()
+                .InstancePerDependency();
+
+            //  register views
+            builder.RegisterAssemblyTypes(AssemblyUtils.GetAppAssembliesArray())
+                .Where(type => type.Name.EndsWith("View"))
+                .Where(type => !(string.IsNullOrWhiteSpace(type.Namespace)) && type.Namespace.EndsWith("Views"))
+                .AsSelf()
+                .InstancePerDependency();
+            _container = builder.Build();
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            var type = typeof(IEnumerable<>).MakeGenericType(service);
+            return _container.Resolve(type) as IEnumerable<object>;
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            if (string.IsNullOrWhiteSpace(key))
+            {
+                if (_container.IsRegistered(service))
+                    return _container.Resolve(service);
+            }
+            else
+            {
+                if (_container.IsRegisteredWithKey(key, service))
+                    return _container.ResolveKeyed(key, service);
+            }
+
+            var msgFormat = "Could not locate any instances of contract {0}.";
+            var msg = string.Format(msgFormat, key ?? service.Name);
+            throw new Exception(msg);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.InjectProperties(instance);
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
