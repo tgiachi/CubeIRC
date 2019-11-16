10edbfcc32570a2fe8db82009d633f013aeeea01
tommaso giachi <squid@stormwind.it>
2019-11-16 14:04:02 +0100

    Added Window collection

--- CubeIRC.Client/ViewModels/ShellViewModel.cs
+++ CubeIRC.Client/ViewModels/ShellViewModel.cs
@@ -4,11 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Caliburn.Micro;
+using CubeIRC.Api.Data;
+using Reactive.Bindings;
 
 namespace CubeIRC.Client.ViewModels
 {
-    public class ShellViewModel: Screen
+    public class ShellViewModel: Screen, IHandle<WindowObject>
     {
+        private IEventAggregator _eventAggregator;
 
+        public ReactiveCollection<WindowObject> Windows { get; set; }
+
+        public ShellViewModel(IEventAggregator eventAggregator)
+        {
+            eventAggregator.Subscribe(this);
+            Windows = new ReactiveCollection<WindowObject>();
+            _eventAggregator = eventAggregator;
+        }
+
+        public void Handle(WindowObject message)
+        {
+            
+        }
     }
 }
